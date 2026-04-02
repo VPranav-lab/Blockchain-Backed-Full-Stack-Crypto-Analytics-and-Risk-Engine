d0286d3cc1443f41d3bffff56f1311d694b29859
VPranav-lab <varangantipranav1@gmail.com>
2026-04-02 18:27:35 +0200

    changes in env.js

--- back-end/Server_3/middleware/authJwt.js
+++ back-end/Server_3/middleware/authJwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { env } = require("../../server/src/config/env");
+const { env } = require("../config/env");
 
 function authJwt(req, _res, next) {
   try {
